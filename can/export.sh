a3dd5dadfd8b3f38c8966a3bb4afa1c8e881dba5
bamboozhang <bamboozhang@tencent.com>
2026-04-29 01:18:15 +0800

    up

---
+++ can/export.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+#cat dnsmasq.log | awk '{
+#
+#    for (i=1; i<=NF; i++) {
+#
+#        if ($i ~ /tesla/) {
+#
+#            print $i
+#
+#        }
+#
+#    }
+#}' | sort -u | awk '{print "server=/" $0 "/223.5.5.5"}' > tesla_domain/tesla_domains_$(date +"%Y%m%d%H%M%S").txt
+
+cat dnsmasq.log | grep query | grep 172.16.0.123 | awk '{print $6}' | sort -u | grep 'tesla'| awk '{print "server=/" $0 "/223.5.5.5"}' > tesla_domain/tesla_domains_$(date +"%Y%m%d%H%M%S").txt
+
+scp -rp dnsmasq.log tesla_domain/* hongbozhang@172.16.0.153:~/tesla_domain/
+
